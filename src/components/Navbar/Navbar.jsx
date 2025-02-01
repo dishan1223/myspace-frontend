4f1f5c1c02858117544a12dafcbecc62c468d664
dishan1223 <dishanishtiaq45+github@gmail.com>
2025-02-01 11:17:59 +0600

    padding changed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 function Navbar() {
   return (
-    <div className="w-full h-16 bg-slate-900 flex justify-between items-center px-18">
+    <div className="w-full h-16 bg-slate-900 flex justify-between items-center px-32">
         <div className="text-2xl font-bold cursor-pointer"><a href="/">MySpace</a></div>
         <div className="flex gap-2">
             <button className="w-20 h-8 bg-neutral-100 text-gray-950 rounded-sm font-medium cursor-pointer hover:bg-slate-200">Log In</button>
