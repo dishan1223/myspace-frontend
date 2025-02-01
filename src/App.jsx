903bdb23e1f2a93f5cfaebd8868d4da9e92674a3
dishan1223 <dishanishtiaq45+github@gmail.com>
2025-02-01 11:00:26 +0600

    initial with navbar

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./components/Navbar/Navbar";
+function App() {
+  return (
+    <div className="w-full h-dvh flex flex-col align-center bg-slate-950">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App;
